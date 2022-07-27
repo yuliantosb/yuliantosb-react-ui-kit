c145e540920ef6b3eab3a5c42a2cf299249fb954
Yulianto Saparudin <yulianto.saparudin@gmail.com>
2022-07-27 22:36:23 +0700

    :zap: update

---
+++ example/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import SayHello from "@yulianto/react-ui-kit";
+
+function App() {
+  return (
+    <div className="App">
+      <SayHello name="Yulianto" />
+    </div>
+  );
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const SayHello = ({ name }: { name: string }): JSX.Element => (
-  <div>Hey {name}, say hello to TypeScript.</div>
+  <div>Hey {name}, say hello to TypeScript this is my first time tho.</div>
 );
 
 export default SayHello;
